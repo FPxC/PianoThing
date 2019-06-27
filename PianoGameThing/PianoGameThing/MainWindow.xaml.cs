087f67324286fb304f37a3bbf7e0f916f3cc8f1c
FPxC <fpcentered@gmail.com>
2019-06-27 10:54:54 +0200

    Piano Thing

---
+++ PianoGameThing/PianoGameThing/MainWindow.xaml.cs
@@ -0,0 +1,127 @@
+﻿using PianoGameThing.Sounds;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PianoGameThing
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SoundPlayer playerC;
+        SoundPlayer playerDb;
+        SoundPlayer playerD;
+        SoundPlayer playerEb;
+        SoundPlayer playerE;
+        SoundPlayer playerF;
+        SoundPlayer playerGb;
+        SoundPlayer playerG;
+        SoundPlayer playerAb;
+        SoundPlayer playerA;
+        SoundPlayer playerBb;
+        SoundPlayer playerB;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            playerC = new SoundPlayer(SoundNames.PianoC);
+            playerC.Load();
+            playerDb = new SoundPlayer(SoundNames.PianoDb);
+            playerDb.Load();
+            playerD = new SoundPlayer(SoundNames.PianoD);
+            playerD.Load();
+            playerEb = new SoundPlayer(SoundNames.PianoEb);
+            playerEb.Load();
+            playerE = new SoundPlayer(SoundNames.PianoE);
+            playerE.Load();
+            playerF = new SoundPlayer(SoundNames.PianoF);
+            playerF.Load();
+            playerGb = new SoundPlayer(SoundNames.PianoGb);
+            playerGb.Load();
+            playerG = new SoundPlayer(SoundNames.PianoG);
+            playerG.Load();
+            playerAb = new SoundPlayer(SoundNames.PianoAb);
+            playerAb.Load();
+            playerA = new SoundPlayer(SoundNames.PianoA);
+            playerA.Load();
+            playerBb = new SoundPlayer(SoundNames.PianoBb);
+            playerBb.Load();
+            playerB = new SoundPlayer(SoundNames.PianoB);
+            playerB.Load();
+        }
+
+        private void C_Click(object sender, RoutedEventArgs e)
+        {
+            playerC.Play();
+        }
+
+        private void Db_Click(object sender, RoutedEventArgs e)
+        {
+            playerDb.Play();
+        }
+
+        private void D_Click(object sender, RoutedEventArgs e)
+        {
+            playerD.Play();
+        }
+
+        private void Eb_Click(object sender, RoutedEventArgs e)
+        {
+            playerEb.Play();
+        }
+        private void E_Click(object sender, RoutedEventArgs e)
+        {
+            playerE.Play();
+        }
+
+        private void F_Click(object sender, RoutedEventArgs e)
+        {
+            playerF.Play();
+        }
+
+        private void Gb_Click(object sender, RoutedEventArgs e)
+        {
+            playerGb.Play();
+        }
+
+        private void G_Click(object sender, RoutedEventArgs e)
+        {
+            playerG.Play();
+        }
+
+        private void Ab_Click(object sender, RoutedEventArgs e)
+        {
+            playerAb.Play();
+        }
+
+        private void A_Click(object sender, RoutedEventArgs e)
+        {
+            playerA.Play();
+        }
+
+        private void Bb_Click(object sender, RoutedEventArgs e)
+        {
+            playerBb.Play();
+        }
+
+        private void B_Click(object sender, RoutedEventArgs e)
+        {
+            playerB.Play();
+        }
+    }
+}
---
+++ PianoGameThing/PianoGameThing/Sounds/SoundNames.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PianoGameThing.Sounds
+{
+    static class SoundNames
+    {
+        static string basePath = System.AppDomain.CurrentDomain.BaseDirectory;
+
+        public static string PianoC = basePath + "\\sounds\\piano-mf-c3.wav";
+        public static string PianoDb = basePath + "\\sounds\\piano-mf-db3.wav";
+        public static string PianoD = basePath + "\\sounds\\piano-mf-d3.wav";
+        public static string PianoEb = basePath + "\\sounds\\piano-mf-eb3.wav";
+        public static string PianoE = basePath + "\\sounds\\piano-mf-e3.wav";
+        public static string PianoF = basePath + "\\sounds\\piano-mf-f3.wav";
+        public static string PianoGb = basePath + "\\sounds\\piano-mf-gb3.wav";
+        public static string PianoG = basePath + "\\sounds\\piano-mf-g3.wav";
+        public static string PianoAb = basePath + "\\sounds\\piano-mf-ab3.wav";
+        public static string PianoA = basePath + "\\sounds\\piano-mf-a3.wav";
+        public static string PianoBb = basePath + "\\sounds\\piano-mf-bb3.wav";
+        public static string PianoB = basePath + "\\sounds\\piano-mf-b3.wav";
+    }
+}
